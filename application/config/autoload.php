20883a6caa5293c458381255f9d6cd875c838c40
Prakash Chhetri <sohanmax02@gmail.com>
2016-09-06 10:55:19 +0545

    Added account related features

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','account_helper');
+$autoload['helper'] = array('url',);
 
 /*
 | -------------------------------------------------------------------
